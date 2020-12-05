98e5482beac97a9f6f265f7e007c57e7377b0feb
wangbingying <wangbingying@kuaishou.com>
2020-12-05 10:28:18 +0800

    feature: setup用法注释完善

--- src/views/composition/Setup.vue
+++ src/views/composition/Setup.vue
@@ -1,26 +1,33 @@
 <template>
-    <div>{{ readersNumber }} {{ book.title }}</div>
+    <h2>{{ state.title }}</h2>
+    <ul style="text-align: left; padding-left: 300px">
+        <li>1.相当于vue2中的beforeCreate与created</li>
+        <li>2.在beforeCreate之后，created之前执行</li>
+        <li>3.setup函数是 Composition API（组合API）的入口</li>
+        <li>4.在setup函数中定义的变量和方法最后都是需要 return 出去的 不然无法再模板中使用</li>
+        <li>5.setup函数只能是同步的不能是异步的。async setup只能用到异步组件中，结合Suspense使用</li>
+    </ul>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { reactive } from 'vue'
+
 export default {
+
     /**
      * 相当于vue2中的beforeCreate与created、在beforeCreate之后，created之前执行
      * @param props
      * @param ctx
      */
     setup(props, ctx) {
-        const readersNumber = ref(0)
-        const book = reactive({ title: 'Vue 3 Guide' })
+        const state = reactive({ title: 'Setup' })
 
         console.log(props)
         console.log(ctx)
 
         // expose to template
         return {
-            readersNumber,
-            book
+            state
         }
     }
 }
