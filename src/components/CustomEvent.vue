b7e5b2ad60a88c674bf08f70da84e9cb424f67a9
wangbingying <wangbingying@kuaishou.com>
2020-12-07 12:51:20 +0800

    feature: 添加自定义事件使用示例

---
+++ src/components/CustomEvent.vue
@@ -0,0 +1,24 @@
+<template>
+    <h5>自定义事件的组件</h5>
+    <button @click="handleClickEvent">点击事件1</button>
+    <button @click="handleClickEvent2">点击事件2</button>
+</template>
+<script>
+    export default {
+        emits:['myEvent','myEvent2'],
+        setup(props, ctx){
+             function handleClickEvent() {
+                console.log('child myEvent clicked!!!!')
+                 ctx.emit('myEvent')
+             }
+            function handleClickEvent2() {
+                console.log('child myEvent2 clicked!!!!')
+                ctx.emit('myEvent2', 'message')
+            }
+             return {
+                 handleClickEvent,
+                 handleClickEvent2
+             }
+        }
+    };
+</script>
--- src/views/composition/Event.vue
+++ src/views/composition/Event.vue
@@ -1,16 +1,31 @@
 <template>
-    event
+    <h2> Event </h2>
+    <ul style="text-align: left; padding-left: 300px">
+        <li>1.vue3新增emits选项，需要将组件发送的自定义事件定义在emits选项中</li>
+        <li>2.setup的第二个参数，context.emit方法中，从第二参数往后，可以传递任意参数</li>
+        <li>3.完全去除了$on、$off、$once三个event emitter实例方法</li>
+        <li>4.实例event bus的方法推荐引入外部事件库，如 <a href="https://github.com/developit/mitt">mitt</a> or <a href="https://github.com/scottcorgan/tiny-emitter">tiny-emitter</a> </li>
+    </ul>
+
+
+    <custom-event @myEvent="handleMyEvent" @myEvent2="handleMyEvent2"/>
 </template>
 
 <script lang="ts">
-import { reactive, ref } from '@vue/reactivity'
+import CustomEvent from '/@components/CustomEvent.vue'
 export default {
+    components: { CustomEvent },
     setup(){
-        // 使用 reactive() 函数定义响应式数据
-        const count = ref(0)
-        const obj = reactive({ count: 0 })
-
-        
+        function handleMyEvent(){
+            console.log('parent myEvent clicked!!!!!')
+        }
+        function handleMyEvent2(params){
+            console.log('parent myEvent2 clicked!!!!!', params)
+        }
+        return {
+            handleMyEvent,
+            handleMyEvent2,
+        }
     }
 };
 </script>
