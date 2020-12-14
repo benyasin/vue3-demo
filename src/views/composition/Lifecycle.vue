16366af374ee5c93e559b09c6321ce022eb3976a
wangbingying <wangbingying@kuaishou.com>
2020-12-14 16:58:50 +0800

    feature: Lifecycle完善

--- src/views/composition/Lifecycle.vue
+++ src/views/composition/Lifecycle.vue
@@ -1,5 +1,19 @@
 <template>
-    lifecycle
+    <h2> Lifecycle </h2>
+    <ul style="text-align: left; padding-left: 300px">
+        <li>1.选项 API 生命周期选项和组合式 API 之间的映射</li>
+        <li>&nbsp;&nbsp; beforeCreate -> use setup()</li>
+        <li>&nbsp;&nbsp; created -> use setup()</li>
+        <li>&nbsp;&nbsp; beforeMount -> onBeforeMount</li>
+        <li>&nbsp;&nbsp; mounted -> onMounted</li>
+        <li>&nbsp;&nbsp; beforeUpdate -> onBeforeUpdate</li>
+        <li>&nbsp;&nbsp; updated -> onUpdated</li>
+        <li>&nbsp;&nbsp; beforeUnmount -> onBeforeUnmount</li>
+        <li>&nbsp;&nbsp; unmounted -> onUnmounted</li>
+        <li>&nbsp;&nbsp; errorCaptured -> onErrorCaptured</li>
+        <li>&nbsp;&nbsp; renderTracked -> onRenderTracked</li>
+        <li>&nbsp;&nbsp; renderTriggered -> onRenderTriggered</li>
+    </ul>
 </template>
 
 <script lang="ts">
